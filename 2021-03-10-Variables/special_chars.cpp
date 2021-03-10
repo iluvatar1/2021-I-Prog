24c98e85eb6c42a6e27d8a70ee0dcd573eadac3e
Iluvatar <woquendo@gmail.com>
2021-03-10 10:48:16 -0500

    Example of special characters in printing

---
+++ 2021-03-10-Variables/special_chars.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(int argc, char *argv[])
+{
+    std::cout << "Hola\nMundo\n";
+    std::cout << "Hola\tMundo\n";
+    std::cout << "Hola\vMundo\n";
+    std::cout << "Hola1234\rMundo\n";
+    std::cout << "Hola\aMundo\n";
+
+    return 0;
+}
