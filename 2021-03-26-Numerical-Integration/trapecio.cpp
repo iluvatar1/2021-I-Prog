028076b67471b7f86581a586fab61e4b0f186062
William Oquendo <woquendo@gmail.com>
2021-03-26 09:32:12 -0500

    Template for trapezoid method

---
+++ 2021-03-26-Numerical-Integration/trapecio.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+double f(double x);
+double trapecio(double a, double b, int npart);
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
+
+double f(double x) {
+    return std::sin(x);
+}
+
+double trapecio(double a, double b, int npart) {
+
+}
