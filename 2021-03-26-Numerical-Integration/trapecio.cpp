55d345f2a9d3ecbd3ab0b9de4f1acd759ceecf39
William Oquendo <woquendo@gmail.com>
2021-03-26 10:07:28 -0500

    Better trapecio

--- 2021-03-26-Numerical-Integration/trapecio.cpp
+++ 2021-03-26-Numerical-Integration/trapecio.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <cmath>
 
 double f(double x);
 double trapecio(double a, double b, int npart);
 
 int main(int argc, char *argv[]) {
-
+    std::cout.precision(15);
+    std::cout.setf(std::ios::scientific);
+    double A = 0.0;
+    double B = M_PI;
+    for (int p = 0; p <= 6; p++){
+        int in = std::pow(10, p);
+        std::cout << in << "\t"
+                  << trapecio(A, B, in) << "\n";
+    }
     return 0;
 }
 
@@ -13,5 +22,14 @@ double f(double x) {
 }
 
 double trapecio(double a, double b, int npart) {
-
+    double h = (b-a)/npart;
+    double result = 0.0;
+    double x = 0;
+    for (int ii = 1; ii < npart; ++ii) {
+        x = a + ii*h;
+        result += f(x);
+    }
+    result += 0.5*(f(a) + f(b));
+    result *= h;
+    return result;
 }
