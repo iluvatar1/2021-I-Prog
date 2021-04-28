f995295aa2d4e9cc28ed4560f7661fab427b2eeb
William Oquendo <woquendo@gmail.com>
2021-04-28 09:54:36 -0500

    Added example with new, delete, and double delete

--- 2021-04-28-DynamicMemory-Structs/dynamic-memory-new-delete-heap.cpp
+++ 2021-04-28-DynamicMemory-Structs/dynamic-memory-new-delete-heap.cpp
@@ -2,15 +2,15 @@
 
 int main(int argc, char *argv[])
 {
-  const int N = 1500000; // this can be read in runtime
+  const int N = 15000000; // this can be read in runtime
   double *data = nullptr;
   data = new double [N];//{0}; // ask for new memory
 
   std::cout << data[N/2] << std::endl;
 
   delete [] data; // return memory
-  //data = nullptr;
-  //delete [] data; // return memory
+  data = nullptr;
+  delete [] data; // return memory
   
   return 0;
 }
