c063cdbaaeb812a9361f4c301aae868a1b811c86
Iluvatar <woquendo@gmail.com>
2021-05-12 09:37:57 -0500

    Primer ejemplo de template

---
+++ 2021-05-12-Templates-Functors/template1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// declaration
+template <class T>
+T getmax(T a, T b);
+
+int main(void)
+{
+    std::cout << getmax(3, 2) << std::endl;
+    std::cout << getmax(-3.5, -2.9) << std::endl;
+    std::cout << getmax(-4.5f, -12.9f) << std::endl;
+    std::cout << getmax('a', 'h') << std::endl;
+    return 0;
+}
+
+// implementation
+template <class T>
+T getmax(T a, T b)
+{
+    T maximum = a;
+    if (b > a) {
+        maximum = b;
+    }
+    return maximum;
+}
